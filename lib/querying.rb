720d54db54c541eae252c505cbfbc4fe8f889a43
Kathleen Carrel <katiecarrel@gmail.com>
2019-02-08 01:27:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECTS authors.name, series.subgenre"
+  "SELECTS authors.name, subgenre.name JOIN "
 end
 
 def select_series_title_with_most_human_characters
